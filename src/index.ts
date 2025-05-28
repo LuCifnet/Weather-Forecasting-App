9b29d0a30f4b1066a154a773c805c6ce89fc5575
kapil <lucifernetwork@gmail.com>
2025-05-28 16:29:28 +0545

    Initial commit: Setup TypeScript project with config constants and scripts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Task 1: Project Setup and Basic Types
+
+type TemperatureUnit = "Celsius" | "Fahrenheit" | "Kelvin";
+
+const API_BASE_URL: string = "https://api.openweathermap.org";
+const DEFAULT_TIMEOUT: number = 5000;
+const ENABLE_CACHING: boolean = true;
+const DEFAULT_UNIT: TemperatureUnit = "Celsius";
+
+console.log("Weather App COnfig:");
+console.log({
+    API_BASE_URL,
+    DEFAULT_TIMEOUT,
+    ENABLE_CACHING,
+    DEFAULT_UNIT
+});
